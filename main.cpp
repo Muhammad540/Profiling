76757c8a0a62a8d1f7f4f39343cf86d5b5c2e5f8
Muhammad540 <thisisdeahmed@gmail.com>
2025-07-15 01:09:00 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cmath>
 #include "haversine_formula.cpp"
 #include "json/json_parser.hpp"
+#include "timer.cpp"
 
 std::string readFile(const std::string& filename) {
     std::ifstream file(filename);
@@ -16,7 +17,24 @@ std::string readFile(const std::string& filename) {
     return buffer.str();
 }
 
+static void PrintTimeElapsed(char const *label, uint64_t TotalTSCElapsed, uint64_t begin, uint64_t end) {
+    uint64_t Elapsed = end - begin;
+    double Percent = 100.0 * ((double)Elapsed / (double)TotalTSCElapsed);
+    std::cout << label << " took " << Elapsed << " TSC ticks (" << Percent << "% of total)" << std::endl;
+}
+
 int main(int ArgCount, char **Args) {
+
+    u64 ProfileBegin = 0;
+    u64 ProfileRead = 0;
+    u64 ProfileMiscSetup = 0;
+    u64 ProfileParseJSON = 0;
+    u64 ProfileSum = 0;
+    u64 ProfileMiscOutput = 0;
+    u64 ProfileEnd = 0;
+
+    ProfileBegin = ReadCPUTimer();
+
     if (ArgCount != 2){
         std::cerr << "Usage: " << Args[0] << " <json_file>" << std::endl;
         return 1;
@@ -24,22 +42,25 @@ int main(int ArgCount, char **Args) {
 
     std::string jsonFile = Args[1];
     try {
+        ProfileRead = ReadCPUTimer();
         // 1. Read the JSON file
         std::string jsonData = readFile(jsonFile);
+        ProfileMiscSetup = ReadCPUTimer();
         std::cout << "---Parsing JSON DATA---" << std::endl;
         // 2. Lexer (Tokenizes the JSON data) 
         Lexer lexer(jsonData);
         // 3. Parser (Parses the JSON data)
         Parser parser(lexer);
         // 4. Parse the JSON data
+        ProfileParseJSON = ReadCPUTimer();
         JsonValue parsedJSON = parser.parse();
         std::cout << "---JSON Parsed Successfully---" << std::endl;
-
         if (parsedJSON.isObject()){
             JsonObject root = parsedJSON.asObject();
             if (root.count("pairs") && root.at("pairs").isArray()){
                 JsonArray pairs = root.at("pairs").asArray();
                 std::cout << "Found " << pairs.size() << " pairs" << std::endl;
+                ProfileSum = ReadCPUTimer();
                 double Sum = 0;
                 double sumCoef = 1.0/(double)pairs.size();
                 for (size_t i = 0; i < pairs.size(); ++i) {
@@ -60,12 +81,25 @@ int main(int ArgCount, char **Args) {
                         Sum += sumCoef * HaversineDistance;
                     }
                 }
+                ProfileMiscOutput = ReadCPUTimer();
                 std::cout << "Sum of Haversine distances: " << Sum << std::endl;
+                ProfileEnd = ReadCPUTimer();
             }
         }
     } catch (const std::exception &err){
         std::cerr << "Error: " << err.what() << std::endl;
     }
+    u64 TotalTSCElapsed = ProfileEnd - ProfileBegin;
+    u64 CPUFreq = EstimateCPUTimerFreq();
+    if (CPUFreq) {
+        std::cout << "Total Time: " << (double)TotalTSCElapsed / (double)CPUFreq << " seconds" << "( CPU Freq: " << CPUFreq / 1000000000 << " GHz)" << std::endl;
+    }
+
+    PrintTimeElapsed("Read", TotalTSCElapsed, ProfileRead, ProfileMiscSetup);
+    PrintTimeElapsed("Misc Setup", TotalTSCElapsed, ProfileMiscSetup, ProfileParseJSON);
+    PrintTimeElapsed("Parse JSON", TotalTSCElapsed, ProfileParseJSON, ProfileSum);
+    PrintTimeElapsed("Sum", TotalTSCElapsed, ProfileSum, ProfileMiscOutput);
+    PrintTimeElapsed("Misc Output", TotalTSCElapsed, ProfileMiscOutput, ProfileEnd);
 
     return 0;
 }
